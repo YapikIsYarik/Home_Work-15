9496914d6d4adbe1a4e0bfd2cbad5a267b667848
YapikIsYarik <99668047+YapikIsYarik@users.noreply.github.com>
2022-08-03 21:01:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 'use strict';
 const UP = document.querySelector('.Up');
 const DOWN = document.querySelector('.Down');
-const TEXT = document.querySelector('.text');
+
 
 function* createIdGenerator() {
    let size = 0;
    while (true) {
-      yield size++;
+      yield ++size;
    };
 };
 
-const idGenerator = createIdGenerator();
+
+
+const fontGenerator = newFontGenerator(14);
 
 function* newFontGenerator(size) {
-   let action = yield size;
+
+
    while (true) {
-      switch (action) {
-         case 'up':
-            action = yield size += 2;
-            break;
-         case 'down':
-            action = yield size -= 2;
-            break;
+      document.querySelector('.text').style.fontSize = size + 'px';
+      let dir = yield size;
+      console.log(dir)
+      if (dir === 'up') {
+         size += 2;
 
-      }
-   }
+      } else if (dir === 'down') {
+         size -= 2;
+      };
+      console.log(size)
+   };
 }
-const fontGenerator = newFontGenerator(14)
 
+console.log(fontGenerator.next().value);
 
-UP.addEventListener('click', () => {
-
-   TEXT.style.fontSize = fontGenerator.next('up').value + 'px';
-})
-DOWN.addEventListener('click', () => {
-   TEXT.style.fontSize = fontGenerator.next('down').value + 'px';
-})
+UP.addEventListener('click', () => fontGenerator.next('up'))
+DOWN.addEventListener('click', () => fontGenerator.next('down'))
 
